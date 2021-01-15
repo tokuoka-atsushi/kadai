43511ea925c0017c78c94fe644702582ee344fe8
LAPTOP-TG76UFOA\tokuo <tokuoka@xenda.co.jp>
2021-01-15 11:28:20 +0900

    20210115

--- vue01/js/main.js
+++ vue01/js/main.js
@@ -1,32 +1,17 @@
+"use strict";
 
-Vue.component('item-list', {
-    data: function() {
-        return {
+document.addEventListener("DOMContentLoaded", getItemList)
+
+function getItemList() {
+    var app = new Vue({
+        el: '#ItemList',
+        data: {
             items: [],
-            test: "test"
-        }
-    },
-    created: function() {
-        axios.get('../../common/js/data.json').then(function(response) {
-            this.items = response.data
-            console.log(this.items);
-        }.bind(this));
-    },
-    template: `
-        <div>
-            <ul class="Items">
-                <li v-for="item in this.items" :key="item.num" class="Items__item">
-                    <img src="http://placehold.jp/24/cc9999/993333/482x289.png" class="Items__img">
-                    <div class="Items__textarea">
-                        <p class="Items__title">{{ item.title }}</p>
-                        <p class="Items__subtext">{{ item.familyName }}</p>
-                    </div>
-                </li>
-            </ul>
-        </div>
-    `
-})
-
-const app = new Vue({
-    el: '#app',
-});
+        },
+        created: function() {
+            axios.get('../../common/js/data.json').then(function(response) {
+                this.items = response.data
+            }.bind(this));
+        },
+    });
+}
