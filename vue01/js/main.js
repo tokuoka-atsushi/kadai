4ba4496c8ce40ee107d31d8907f46e533e8663ee
LAPTOP-TG76UFOA\tokuo <tokuoka@xenda.co.jp>
2021-02-03 15:11:32 +0900

    修正#comment-57264907

--- vue01/js/main.js
+++ vue01/js/main.js
@@ -7,15 +7,49 @@ function getItemList() {
         el: '#ItemList',
         data: {
             items: [],
+            matchMokuzou: {
+                title: '木造',
+                subTitle: 'WOODEN HOUSE',
+                list: []
+            },
+            matchHomes: {
+                title: 'パナソニック ホームズの住まい',
+                subTitle: 'Panasonic Homes HOUSE',
+                list: []
+            },
+            matchRc: {
+                title: 'RC・鉄骨造',
+                subTitle: 'RC・STEEL FRAME',
+                list: []
+            },
+            limit: 10,
         },
         computed: {
             itemLength: function() {
                 return this.items.length;
-            }
+            },
+            matchMokuzou_limited: function() {
+                return this.matchMokuzou.list.slice(0, this.limit)
+            },
+            matchHomes_limited: function() {
+                return this.matchHomes.list.slice(0, this.limit)
+            },
+            matchRc_limited: function() {
+                return this.matchRc.list.slice(0, this.limit)
+            },
         },
         created: function() {
             axios.get('../../common/js/data.json').then(function(response) {
                 this.items = response.data
+                this.matchMokuzou.list = response.data.filter((item) => {
+                    return item.typeText === '木造';
+                });
+                this.matchHomes.list = response.data.filter((item) => {
+                    return item.typeText === 'パナソニック ホームズの住まい';
+                });
+                this.matchRc.list = response.data.filter((item) => {
+                    return item.typeText === 'RC・鉄骨造';
+                });
             }.bind(this));
         },
     });
